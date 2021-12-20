017d4aa7e08c3083b4cc9dd8cad560b1a88b3c22
Gomulko <rafa.gomu1994@gmail.com>
2021-12-20 12:50:58 +0100

    set port on 3001

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
